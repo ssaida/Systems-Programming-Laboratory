8852059868a760f7c415a2630715a9614dfa36c4
Samer Saida <ssaida@gmail.com>
2025-06-28 14:31:59 +0300

    Add files via upload

--- Eran-Petel/count.c
+++ Eran-Petel/count.c
@@ -1,29 +1,26 @@
 /*
  * count.c
  *
- *  Created on: Jun 8, 2025
+ *  Created on: Jun 27, 2025
  *      Author: user
  */
 
 #include <stdio.h>
 
-
-
 int main(){
 	int c;
 	int ns, nt, nl;
 	ns = nt = nl = 0;
-	while((c = getchar()) != EOF){
+
+	while( (c = getchar()) != EOF){
+		if(c == ' ')
+			ns++;
 		if(c == '\n')
 			nl++;
 		if(c == '\t')
 			nt++;
-		if(c == ' ')
-			ns++;
 	}
-	printf("------------------\n");
-	printf("Total Spaces: %d\n", ns);
-	printf("Total Tabs: %d\n", nt);
-	printf("Total lines: %d\n", nl);
+	printf("----------------------");
+	printf("\nTabs: %d\nSpaces: %d\nNewLines: %d\n", nt, ns, nl);
 	return 0;
 }
