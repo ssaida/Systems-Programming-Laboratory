eff32e665d9800273902373be6ad1e1b8b61444f
Samer Saida <ssaida@gmail.com>
2025-08-05 12:03:30 +0300

    Add files via upload

---
+++ BitWise Operators/BitWise Operators/intro.c
@@ -0,0 +1,22 @@
+/*
+ * intro.c
+ *
+ *  Created on: Jul 5, 2025
+ *      Author: user
+ */
+#include <stdio.h>
+
+int main(){
+
+	int a = 1101;
+	int b = 0x123;
+	int c = 045;
+	int d = 32467;
+
+	printf("a: %d\nb: %d\nc: %d\nd: %d\n", a, b, c, d);
+
+	return 0;
+}
+
+
+
---
+++ BitWise Operators/Bitwise Operators/intro.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+  printf("Hello Samer Saida\n\n");
+  return 0;
+}
